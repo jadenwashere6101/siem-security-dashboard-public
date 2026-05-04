a519a0e5b65260ba4420b84f0af48c02d657e50a
Jaden Gomez <jadengomez067@gmail.com>
2026-05-03 20:22:37 -0400

    Move API guards into helpers package

--- backend_ingest_routes.py
+++ backend_ingest_routes.py
@@ -8,7 +8,7 @@ from adapters.azure_insights_adapter import (
 )
 from adapters.nginx_adapter import parse_nginx_access_log_line
 from adapters.otel_adapter import normalize_otel_telemetry
-from backend_api_guards import require_api_key, require_azure_api_key, require_otel_api_key
+from helpers.api_guards import require_api_key, require_azure_api_key, require_otel_api_key
 from backend_db import get_db_connection
 from backend_extensions import limiter
 from backend_ingest_engine import ingest_normalized_event
